20a5cc90dbb9c499dc8240b2149d4b96746c332a
taa5804 <taa5804@naver.com>
2026-07-22 11:31:18 +0900

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -7,10 +7,7 @@ importScripts(
 );
 
 firebase.initializeApp({
-  const firebaseConfig = {
   apiKey: "AIzaSyB3nZdmUrj2wsKPQpOV8dlso7iI7fZo0Hc",
-  ...
-}
   authDomain: "wooriapt-carqr.firebaseapp.com",
   projectId: "wooriapt-carqr",
   storageBucket: "wooriapt-carqr.firebasestorage.app",
