d341b7630d3456c6d839e9687f834c4809479811
taa5804 <taa5804@naver.com>
2026-09-24 14:42:06 +0900

    Update Supabase URL and key in broker-sitemap.js

--- api/broker-sitemap.js
+++ api/broker-sitemap.js
@@ -14,10 +14,10 @@
 */
 
 const SUPABASE_URL =
-  "https://dcysjuxyjqtvkihdsjvv.supabase.co";
+  "https://wpshlmjjsscmlasqtapa.supabase.co";
 
 const SUPABASE_KEY =
-  "sb_publishable_RZBX7u1v8MLBCfEJT0-eRg_jPcIulG2";
+  "sb_publishable_3QLYewR-TXBd1mNfte6OJg_kre42L7K";
 
 const SITE_ORIGIN =
   "https://www.wooriapt.app";
